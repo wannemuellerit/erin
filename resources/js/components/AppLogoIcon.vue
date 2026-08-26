fd1e51f92597a30b675e5da32cac6e1fce6d1743
Dave <22962384+wannemuellerit@users.noreply.github.com>
2026-08-26 11:16:56 +0200

    fix: brand page titles and favicon as Faden (#124)

--- resources/js/components/AppLogoIcon.vue
+++ resources/js/components/AppLogoIcon.vue
@@ -20,10 +20,7 @@ defineProps<Props>();
         v-bind="$attrs"
     >
         <rect width="40" height="40" rx="12" fill="#2563EB" />
-        <path
-            d="M11 11.5h17.6v5H17v3h10.2v4.7H17v3.3h12v5H11v-21Z"
-            fill="white"
-        />
+        <path d="M11 11.5h18v5H17v4h10.2v5H17v7H11v-21Z" fill="white" />
         <circle cx="30.5" cy="11" r="4.5" fill="#F97316" />
     </svg>
 </template>
